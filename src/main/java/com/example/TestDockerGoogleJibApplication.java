6f19a8d8d36afc1ab9625ce9232678bcc7eda338
otereshchenko <xander.tereschenko@gmail.com>
2023-12-01 23:25:17 +0200

    Init commit

---
+++ src/main/java/com/example/TestDockerGoogleJibApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDockerGoogleJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDockerGoogleJibApplication.class, args);
+    }
+
+}
